c85bc631c51fdd6b3953c194c238d425bcb65213
bhavitha009 <bhavithagari1342@gmail.com>
2026-02-23 10:55:59 +0530

    UC3: Palindrome check using String reverse

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -16,6 +16,15 @@ public class PalindromeApp {
                 (hardcoded.equals("madam") ? "Palindrome\n" : "Not Palindrome\n"));
 
 
+        //UC3
+        String input1 = "Ardra";
+        String reverse = new StringBuilder(input1).reverse().toString();
+        System.out.println("String Reverse Method: " +
+                (input1.equalsIgnoreCase(reverse) ? "Palindrome\n" : "Not Palindrome\n"));
+
+
+
+
     }
 
 
