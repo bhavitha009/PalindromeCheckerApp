8f8956a9b56fe15b09dae8dbd3d15ffe773b46ff
JAYANTMANDIA <mandiajayant@gmail.com>
2026-02-23 09:14:45 +0530

    Base application setup

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+
+public class PalindromeCheckerApp{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Palindrome Checker App");
+        System.out.println("Application Name : Palindrome Checker");
+        System.out.println("Version : 1.0");
+        System.out.println("Use Case :  Application Entry & Welcome Message\n");
+    }
+}
