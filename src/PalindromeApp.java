f88f6ca972ddd285ce8d56633cd15ec943f93179
bhavitha009 <bhavithagari1342@gmail.com>
2026-02-23 11:00:28 +0530

    UC7: Deque based optimized palindrome check

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -97,6 +97,35 @@ public class PalindromeApp {
 
 
 
+        //UC7
+        String input5 = "Refer";
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : input5.toLowerCase().toCharArray()) {
+            deque.add(c);
+        }
+
+        boolean dequeCheck = true;
+        while (deque.size() > 1) {
+            if (!deque.pollFirst().equals(deque.pollLast())) {
+                dequeCheck = false;
+                break;
+            }
+        }
+
+        System.out.println("Deque Method: " +
+                (dequeCheck ? "Palindrome\n" : "Not Palindrome\n"));
+
+
+
+
+
+
+
+
+
+
+
     }
 
 
