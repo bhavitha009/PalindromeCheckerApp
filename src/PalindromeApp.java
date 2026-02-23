537cd4f6c54d6340d626c08151586dfa828478b3
bhavitha009 <bhavithagari1342@gmail.com>
2026-02-23 10:53:46 +0530

    UC2: Hardcoded palindrome check

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -8,7 +8,16 @@ public class PalindromeApp {
         System.out.println("Welcome to Palindrome Checker App");
         System.out.println("Application Name : Palindrome Checker");
         System.out.println("Version : 1.0\n");
+
+
+        //UC2
+        String hardcoded = "madam";
+        System.out.println("Hardcoded Check: " +
+                (hardcoded.equals("madam") ? "Palindrome\n" : "Not Palindrome\n"));
+
+
     }
+
 
 
 }
