5c680260ec01be638c4cf40fa460253686321b06
bhavitha009 <bhavithagari1342@gmail.com>
2026-03-10 09:19:02 +0530

    Fix UC2: Only UC2 code

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -4,10 +4,6 @@ public class PalindromeApp {
 
     public static void main(String[] args) {
 
-        //UC1
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Application Name : Palindrome Checker");
-        System.out.println("Version : 1.0\n");
 
 
         //UC2
