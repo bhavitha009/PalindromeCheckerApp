d5cc286056bf867659a81e944e6ca28add6e60af
bhavitha009 <bhavithagari1342@gmail.com>
2026-03-10 09:19:30 +0530

    Fix UC3: Only UC3 code

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -4,17 +4,6 @@ public class PalindromeApp {
 
     public static void main(String[] args) {
 
-        //UC1
-        System.out.println("Welcome to Palindrome Checker App");
-        System.out.println("Application Name : Palindrome Checker");
-        System.out.println("Version : 1.0\n");
-
-
-        //UC2
-        String hardcoded = "madam";
-        System.out.println("Hardcoded Check: " +
-                (hardcoded.equals("madam") ? "Palindrome\n" : "Not Palindrome\n"));
-
 
         //UC3
         String input1 = "Ardra";
