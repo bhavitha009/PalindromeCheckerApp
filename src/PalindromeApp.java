2c552d28a17581f838c20d872a0b8ee88326bf1c
bhavitha009 <bhavithagari1342@gmail.com>
2026-03-10 10:10:14 +0530

    UC5: Stack based palindrome check

--- src/PalindromeApp.java
+++ src/PalindromeApp.java
@@ -5,22 +5,34 @@ public class PalindromeApp {
     public static void main(String[] args) {
 
 
-        String input2 = "AlulA";
-        char[] arr = input2.toCharArray();
-        boolean isPalindrome = true;
 
-        int start = 0, end = arr.length - 1;
-        while (start < end) {
-            if (arr[start] != arr[end]) {
-                isPalindrome = false;
-                break;
-            }
-            start++;
-            end--;
+
+
+
+        String input3 = "Level";
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : input3.toLowerCase().toCharArray()) {
+            stack.push(c);
         }
 
-        System.out.println("Character Array Method: " +
-                (isPalindrome ? "Palindrome\n" : "Not Palindrome\n"));
+        boolean stackCheck = true;
+        for (char c : input3.toLowerCase().toCharArray()) {
+            if (c != stack.pop()) {
+                stackCheck = false;
+                break;
+            }
+        }
+
+        System.out.println("Stack Method: " +
+                (stackCheck ? "Palindrome\n" : "Not Palindrome\n"));
+
+
+
+
+
+
+
 
 
 
